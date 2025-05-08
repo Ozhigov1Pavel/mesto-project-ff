ff07511ed074571ca7b285f27b98d0c30074a8f7
Ozhigov Pavel <ozhigovpasha@gmail.com>
2025-05-08 17:02:20 +0700

    createCards and delete

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,23 @@
-// @todo: Темплейт карточки
+function cards(cardData, deleteCall) {
+  const cardCreate = document.querySelector("#card-template");
+  const cardElement = cardCreate.content.querySelector(".places__item").cloneNode(true);
+  cardElement.querySelector(".card__image").src = cardData.link;
+  cardElement.querySelector(".card__image").alt = cardData.name;
+  cardElement.querySelector(".card__title").textContent = cardData.name;
 
-// @todo: DOM узлы
+  const deleteBtn = cardElement.querySelector(".card__delete-button");
+  deleteBtn.addEventListener("click", deleteCall);
 
-// @todo: Функция создания карточки
+  return cardElement;
+}
 
-// @todo: Функция удаления карточки
+function deleteCard(event) {
+  event.target.closest(".places__item").remove();
+}
 
-// @todo: Вывести карточки на страницу
+const placesList = document.querySelector(".places__list");
+
+initialCards.forEach(function (cardData) {
+  const card = cards(cardData, deleteCard);
+  placesList.append(card);
+});
